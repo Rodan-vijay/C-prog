120e704e8a1e488e50ff0b0d74fc4025e2478e0c
Rodan-vijay <166347326+Rodan-vijay@users.noreply.github.com>
2024-05-07 22:11:24 +0530

    Update New Text Document.c

--- New Text Document.c
+++ New Text Document.c
@@ -36,3 +36,43 @@ sort(std);
 for(int i=0;i<n;i++)
 printf("%s %s %f\n",std[i].roll,std[i].name,std[i].cgpa);
 }
+----------------------------------------
+//File handling
+#include<stdio.h>
+#include<string.h>
+struct student
+{
+char roll[10],name[30];
+float cgpa;
+}temp;
+void sort(struct student std[],int n)
+{
+for (int i=0;i<n;i++)
+	for(int j=0;j<n-i-1;j++)
+		if(strcmp(std[j].roll,std[j+1].roll)>0)
+{
+temp=std[j];
+std[j]=std[j+1];
+std[j+1]=temp;
+}
+
+}
+void main()
+{int n=0;
+//printf("Enter the number of students in the list : ");
+//scanf("%d",&n);
+struct student std[100];
+FILE *fptr;
+fptr=fopen("std.txt","r");
+int n=0,i=0;
+while(fscanf(fptr,"%s %s %f",&std[i].roll,&std[i].name,&std[i].cgpa)!=EOF)
+{//printf("%s %s %f",std[i].roll,std[i].name,std[i].cgpa);
+n++;
+i++;
+}
+//printf("%d",n);
+sort(std,n);
+for(int i=0;i<n;i++)
+printf("%s %s %f\n",std[i].roll,std[i].name,std[i].cgpa);
+}
+
